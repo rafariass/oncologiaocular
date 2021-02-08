9530f4da458ccfd0724a35eab9428b89f36c9ac0
Raul Farias S <ra.farias.s@outlook.com>
2021-02-08 13:43:07 -0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,6 +64,7 @@
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
+                                        <i class="fas fa-times"></i>
                                         {{ __('Logout') }}
                                     </a>
 
